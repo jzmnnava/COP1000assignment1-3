6c9e6b8263489b6e7fa22ce6734a388502b86b2a
jzmnnava <146144314+jzmnnava@users.noreply.github.com>
2024-09-13 20:46:50 +0000

    Added what the code does

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+#This program takes the value of the item and will produce the new value
+
 Value = float(input("Enter the value of the item: "))
 Result = Value * 10
 print("The value of the item is: " + str(Result))
